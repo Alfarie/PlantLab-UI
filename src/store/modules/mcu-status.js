89471564a7292e86d8d11d8e2fb8c298837549b5
Farhan Abuhazan <farhan.abuhazan@gmail.com>
2018-04-27 19:02:49 +0700

    datetime component

--- src/store/modules/mcu-status.js
+++ src/store/modules/mcu-status.js
@@ -13,6 +13,15 @@ const state = {
             crt: 0,
             max: 3600
         }
+    },
+    ecStatus: {
+
+    },
+    co2Status: {
+        
+    },
+    phStatus: {
+
     }
 }
 
@@ -22,8 +31,20 @@ const getters = {
             gpio: state.gpio,
             memory: state.memory,
             paracc: state.paracc,
-            waterProcess: state.waterProcess
+            waterProcess: state.waterProcess,
+            ecStatus: state.ecStatus,
+            co2Status: state.co2Status,
+            phStatus: state.phStatus
         }
+    },
+    ecStatus(state){
+        return state.ecStatus;
+    },
+    phStatus(state){
+        return state.phStatus;
+    },
+    co2Status(state){
+        return state.co2Status;
     }
 }
 
@@ -39,11 +60,22 @@ const mutations ={
     },
     SOCKET_WATER_PROCESS:(state, data)=>{
         state.waterProcess = data[0];
+    },
+    SOCKET_EC_STATUS: (state,data)=>{
+        state.ecStatus = data[0]
+    },
+    SOCKET_PH_STATUS: (state,data)=>{
+        state.phStatus = data[0]
+    },
+    SOCKET_CO2_STATUS: (state,data)=>{
+        state.co2Status = data[0]
     }
 }
 
 const actions = {
-   
+    updateDateTime(context, payload){
+        axios.post('setting/datetime', payload)
+    }
 }
 
 export default{
