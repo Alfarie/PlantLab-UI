6cec6b86031ae60bdbf1f26cd6ddff94ee0b30a2
Farhan Abuhazan <farhan.abuhazan@gmail.com>
2018-05-09 11:07:57 +0700

    predefine control model

---
+++ src/models/model-control.js
@@ -0,0 +1,118 @@
+export default  [
+    {
+        "ch": 1,
+        "mode": 1,
+        "sensor": 0,
+        "manual": {
+            "status": 0
+        },
+        "timer": {
+            "size": 3,
+            "mode":0,
+            "list": [ [0,60], [120,180], [300,360]]
+        },
+        "setpoint":{
+            "setpoint": 3.5,
+            "working": 15,
+            "detecting": 15
+        },
+        "setbound": {
+            "upper": 40,
+            "lower": 60
+        },
+        "irrigation": {
+            "soil_upper": 60,
+            "soil_lower": 40,
+            "par_accum": 1.0,
+            "working": 30,
+            "mode": 0
+        }
+    },
+    {
+        "ch": 3,
+        "mode": 3,
+        "sensor": 0,
+        "manual": {
+            "status": 0
+        },
+        "timer": {
+            "size": 3,
+            "mode":0,
+            "list": [ [0,60], [120,180], [300,360]]
+        },
+        "setpoint":{
+            "setpoint": 3.5,
+            "working": 15,
+            "detecting": 15
+        },
+        "setbound": {
+            "upper": 1000,
+            "lower": 1200
+        },
+        "irrigation": {
+            "soil_upper": 60,
+            "soil_lower": 40,
+            "par_accum": 1.0,
+            "working": 30,
+            "mode": 0
+        }
+    },
+    {
+        "ch": 3,
+        "mode": 2,
+        "sensor": 4,
+        "manual": {
+            "status": 0
+        },
+        "timer": {
+            "size": 3,
+            "mode":0,
+            "list": [ [0,60], [120,180], [300,360]]
+        },
+        "setpoint":{
+            "setpoint": 1.5,
+            "working": 15,
+            "detecting": 15
+        },
+        "setbound": {
+            "upper": 40,
+            "lower": 60
+        },
+        "irrigation": {
+            "soil_upper": 60,
+            "soil_lower": 40,
+            "par_accum": 1.0,
+            "working": 30,
+            "mode": 0
+        }
+    },
+    {
+        "ch": 4,
+        "mode": 2,
+        "sensor": 0,
+        "manual": {
+            "status": 0
+        },
+        "timer": {
+            "size": 3,
+            "mode":0,
+            "list": [ [0,60], [120,180], [300,360]]
+        },
+        "setpoint":{
+            "setpoint": 7.5,
+            "working": 15,
+            "detecting": 15
+        },
+        "setbound": {
+            "upper": 40,
+            "lower": 60
+        },
+        "irrigation": {
+            "soil_upper": 60,
+            "soil_lower": 40,
+            "par_accum": 1.0,
+            "working": 30,
+            "mode": 0
+        }
+    }
+]
